24a0bde1a0457dd4bbd065f438a3bb5b0ee53e11
71977_Mayur <mayurk1997.mk@gmail.com>
2023-08-30 14:12:16 +0530

    commited

---
+++ employee-adminstration-frontend/src/page/AboutUs.jsx
@@ -0,0 +1,11 @@
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>An Employee Administration System Project in Spring Boot and React empowers administrators to efficiently manage departments, managers, employees, and salary information. This project delves into the functionalities of an EAS wherein directors can effortlessly add departments and assign managers liable for overseeing worker control. Additionally, the gadget allows the recording and monitoring of revenue information for each worker. Discover how this comprehensive solution enhances organizational efficiency, simplifies administrative tasks, and guarantees accurate control of worker records and compensation. Streamline your team of workers control tactics and optimize productivity with an Employee Administration System tailored to satisfy the specific desires of your agency.
+
+</b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ employee-adminstration-frontend/src/page/ContactUs.jsx
@@ -0,0 +1,12 @@
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>Mayur Kasbe</b>
+      <p>mayurkasbe.100@gmail.com</p>
+      <b>Sahil Totade</b>
+      <p>sahiltotade03@gmail.com</p>
+    </div>
+  );
+};
+
+export default ContactUs;
