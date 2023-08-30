a2aea8e96e69bee7ddd8a13aa9cf9e927e62e45b
71977_Mayur <mayurk1997.mk@gmail.com>
2023-08-30 14:10:04 +0530

    commited

---
+++ employee-administration_backend/employee-salary-service/src/main/java/com/employee/salary/service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.employee.salary.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.employee.salary.service.dto.CommonApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(SalaryNotFoundException.class)
+	public ResponseEntity<CommonApiResponse> handleUserNotFoundException(SalaryNotFoundException ex) {
+		String responseMessage = ex.getMessage();
+		
+		CommonApiResponse apiResponse = new CommonApiResponse();
+		apiResponse.setResponseMessage(responseMessage);
+		apiResponse.setStatus(HttpStatus.NOT_FOUND);
+		apiResponse.setSuccess(false);
+		return new ResponseEntity<CommonApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+		
+	}
+
+}
